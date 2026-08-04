93a7bfdf1b3dcdd8e8436c73abcc02e0112bae30
Mithrand <mithrand0@users.noreply.github.com>
2026-08-04 16:36:42 +0200

    shutdown hooks

--- run-client.ps1
+++ run-client.ps1
@@ -1,15 +1,18 @@
 # Set your SteamCMD install directory here
 $logFile = $args[0]
 
+# add shutdown hook
+$pattern = "shutting down client"
+echo "$pattern; quit" > reactivedrop/cfg/shutdown.cfg
+
 # run client and wait for output
-cmd /c start /wait reactivedrop.exe -textmode -condebug -conclearlog +exec test
+cmd /c start /wait reactivedrop.exe -textmode -condebug -conclearlog +exec test +exec shutdown
 
 # move console logfile
 mv -Force reactivedrop/console.log $logFile
 Get-Content $logFile -Tail 50
 
 # check for errors
-$pattern = 'Shutdown function ShutdownMixerControls() not in list!!!'
 if (-not (Select-String -Path $logFile -SimpleMatch $pattern)) {
     Write-Error "Shutdown line missing in $logFile"
     exit 1
--- run-server.ps1
+++ run-server.ps1
@@ -1,16 +1,19 @@
 # Set your SteamCMD install directory here
 $logFile = $args[0]
 
+# add shutdown hook
+$pattern = "shutting down server"
+echo "$pattern; quit" > reactivedrop/cfg/shutdown.cfg
+
 # run client and wait for output
 cmd /c start /wait srcds_console.exe -console -condebug -conclearlog `
-    -game reactivedrop -noassert +sv_lan 1 +exec test
+    -game reactivedrop -noassert +sv_lan 1 +exec test +exec shutdown
 
 # move console logfile
 mv -Force reactivedrop/console.log $logFile
 Get-Content $logFile -Tail 50
 
 # check for errors
-$pattern = 'Shutdown function ShutdownMixerControls() not in list!!!'
 if (-not (Select-String -Path $logFile -SimpleMatch $pattern)) {
     Write-Error "Shutdown line missing in $logFile"
     exit 1
